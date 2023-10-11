a73b78e3121d87a8c6edad30dc9a79d3657128c5
VISHALI-V <65996784+VISHALI-V@users.noreply.github.com>
2023-10-11 15:27:17 +0530

    Update orders_def.cpp

--- orders_def.cpp
+++ orders_def.cpp
@@ -4,8 +4,7 @@ using namespace std;
 
 orders::orders()
 {
-    int i;
-    for(i=0;i<5;i++)
+    for(int i=0;i<5;i++)
     {
         strcpy(ord[i].name,"\0");
         ord[i].price=0;
@@ -18,8 +17,7 @@ orders::orders()
 void orders::calc_amt()
 {
     float sum=0;
-    int i;
-    for(i=0;i<5;i++)
+    for(int i=0;i<5;i++)
     {
         sum=sum + ord[i].no*ord[i].price;
     }
